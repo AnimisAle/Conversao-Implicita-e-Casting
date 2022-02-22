48b3b495f89692f16943cd0f86e4a1faaa435cf9
Lucas de Lima <fenannaa@gmail.com>
2022-02-22 17:37:14 -0300

    Confirmar

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+
+namespace App1{
+
+	class program{
+static void Main(string[] args){
+
+	double a;
+
+	int b; 
+
+	a =  5.5;
+
+	b = (int) a;
+
+	System.Console.WriteLine("Valor de a: " + a);
+	System.Console.WriteLine("Valor de b: " + b);
+
+
+	System.Console.WriteLine("------------------");
+
+	int c = 5;
+	int d = 2; 
+
+	double resultado = (double)c/d;
+
+	System.Console.WriteLine(resultado);
+}
+
+	}
+}
